26b64980733b81d74d76d671388f22ee5639444d
ennmichael <ennmichael@gmail.com>
2017-08-12 00:19:44 +0200

    Added main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 
 import sdl2_wrap
-import mouse
+import detection
 
 
 TRACKING_DISTANCE = 10
@@ -10,11 +10,14 @@ TRACKING_DISTANCE = 10
 
 def main():
     with sdl2_wrap.Window('line detector', (500, 500)) as window, sdl2_wrap.Renderer(window) as renderer:
-        event_loop = sdl2_wrap.EventLoop()
-
-        m = mouse.Mouse(event_loop, TRACKING_DISTANCE)
-
+        event_loop = sdl2_wrap.EventLoop.with_default_quit_handling()
         window.show()
+        renderer.color = sdl2_wrap.colors.WHITE
+        detection.start_main_loop(
+            event_loop=event_loop,
+            renderer=renderer,
+            tracking_distance=TRACKING_DISTANCE,
+            stroke_color=sdl2_wrap.colors.BLACK)
 
 
 if __name__ == '__main__':
