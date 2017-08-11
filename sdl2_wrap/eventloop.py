c8d6e1f76b9c419b6e3a7f0b4b9741ecea06c65b
ennmichael <ennmichael@gmail.com>
2017-08-11 22:37:00 +0200

    Not garbage anymore

--- sdl2_wrap/eventloop.py
+++ sdl2_wrap/eventloop.py
@@ -3,7 +3,7 @@
 
 import sdl2.ext
 from . import events
-from typing import Callable, Any, List, NamedTuple
+from typing import Callable, Any, List, Tuple
 
 
 class ControlFlowException(Exception):
@@ -14,31 +14,36 @@ class QuitEventLoop(ControlFlowException):
     pass
 
 
+EventCallback = Callable[[sdl2.SDL_Event], None]
+
+
 class EventLoop:
 
     @staticmethod
     def with_default_quit_handling() -> Any:
 
-        def default_quit_callback() -> None:
+        def default_quit_callback(_: sdl2.SDL_Event) -> None:
             raise QuitEventLoop
 
-        return EventLoop().add_event(events.UserQuit(default_quit_callback))
+        event_loop = EventLoop()
+        event_loop.on(event=events.user_quit, callback=default_quit_callback)
+        return event_loop
 
     def __init__(self) -> None:
-        self.__events: List[events.Event] = []
+        self.__events_and_callbacks: List[Tuple[events.Event, EventCallback]] = []
 
-    def add_event(self, event: events.Event) -> Any:
-        self.__events.append(event)
-        return self
+    def on(self, *, event: events.Event, callback: EventCallback) -> Any:
+        self.__events_and_callbacks.append((event, callback))
 
     def run(self) -> None:
         while True:
-            for raw_event in sdl2.ext.get_events():
+            for sdl_event in sdl2.ext.get_events():
                 try:
-                    self.__dispatch_events(raw_event)
+                    self.__dispatch_event(sdl_event)
                 except QuitEventLoop:
                     return
 
-    def __dispatch_events(self, raw_event: sdl2.SDL_Event) -> None:
-        for event in self.__events:
-            event.dispatch(raw_event)
+    def __dispatch_event(self, sdl_event: sdl2.SDL_Event) -> None:
+        for event, callback in self.__events_and_callbacks:
+            if event(sdl_event):
+                callback(sdl_event)
